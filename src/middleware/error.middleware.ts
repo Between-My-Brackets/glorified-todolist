5f687db81387517d56d44b1bb46789349a20c4a5
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-02-04 13:32:45 +0530

    slight changes and the newman generate document on the api endpoints

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -6,6 +6,12 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
+    if (err.name === "CastError") {
+        return res.status(404).json({
+            message: "Resource not found"
+        });
+    }
+
     console.error(err);
     res.status(500).json({
         error: err.message ||
